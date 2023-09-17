5c8175535413c31fdf8d1828d6a5fa54e1f8e43b
cuongkoi <cuongkoi1406@gmail.com>
2023-09-17 17:08:27 +0700

    fix

--- Component/PlayingBar/PlayingBar.php
+++ Component/PlayingBar/PlayingBar.php
@@ -1,11 +1,16 @@
-  <!DOCTYPE html>
-  <html lang="en">
-
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/ZingMP3/Component/PlayingBar/PlayingBar.css?v=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="/ZingMP3/Component/PlayingBar/PlayingBar.css?v=1.0"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   </head>
 
@@ -15,7 +20,11 @@
         <div class="player_controls-left">
           <div class="player_controls-media">
             <div class="media_left">
-              <img class="media-avatar" src="https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/avatars/5/d/a/9/5da92b54155ac844f9dac366057cbcde.jpg" alt="" />
+              <img
+                class="media-avatar"
+                src="https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/avatars/5/d/a/9/5da92b54155ac844f9dac366057cbcde.jpg"
+                alt=""
+              />
             </div>
             <div class="media_center">
               <div class="media_music">Người Lạ Thoáng Qua (Lofi Version)</div>
@@ -55,9 +64,13 @@
           <div class="player_bottom">
             <p class="playing_time-left">0:00</p>
             <div class="playing_time-up2 progress-area">
-              <div class="progress-bar" style="width:0 %"></div>
+              <div class="progress-bar" style="width: 0 %"></div>
               <div style="width: 0px; height: 0px">
-                <audio src="http://api.mp3.zing.vn/api/streaming/audio/ZUBEFEU7/320" preload="auto" style="width: 100%; height: 100%"></audio>
+                <audio
+                  src="http://api.mp3.zing.vn/api/streaming/audio/ZUBEFEU7/320"
+                  preload="auto"
+                  style="width: 100%; height: 100%"
+                ></audio>
               </div>
             </div>
             <p class="playing_time-right">0:00</p>
@@ -67,7 +80,14 @@
           <div class="player_volume playing_volume">
             <div class="player_btn"><i class="fa-solid fa-volume-low"></i></div>
             <div class="playing_volume-input">
-              <input class="transition-all" id="inputVolume" type="range" min="0" max="100" value="30" />
+              <input
+                class="transition-all"
+                id="inputVolume"
+                type="range"
+                min="0"
+                max="100"
+                value="30"
+              />
             </div>
           </div>
           <div class="player_device-column"></div>
@@ -79,30 +99,35 @@
         </div>
       </div>
     </div>
-    <audio id="SongPlaying" hidden controls src="../../Component/assets/phequa.mp3"></audio>
+    <audio
+      id="SongPlaying"
+      hidden
+      controls
+      src="../../Component/assets/phequa.mp3"
+    ></audio>
 
     <!-- <script>
    
     </script> -->
-      <script>
-           // Khai báo để gọi các thẻ 
+    <script>
+      // Khai báo để gọi các thẻ
       const timeLeft = document.querySelector(".playing_time-left");
       const timeRight = document.querySelector(".playing_time-right");
-      const btnHeart = document.querySelector(".no-hearted")
-      const btnHearted = document.querySelector(".hearted")
+      const btnHeart = document.querySelector(".no-hearted");
+      const btnHearted = document.querySelector(".hearted");
       const btnPlay = document.querySelector(".player_btn.relative i");
       const songPlaying = document.querySelector("#SongPlaying");
-      const btnVolume = document.querySelector("#inputVolume")
+      const btnVolume = document.querySelector("#inputVolume");
       const volumeIcon = document.querySelector(".fa-volume-low");
       // Add a variable to track the paused state of the audio element
 
       // Thả tim
-      $(document).ready(function() {
-            $(".media_right-btn.player_btn").click(function() {
-                $(".fa-regular.fa-heart").toggle();
-                $(".fa-solid.fa-heart").toggle();
-            });
+      $(document).ready(function () {
+        $(".media_right-btn.player_btn").click(function () {
+          $(".fa-regular.fa-heart").toggle();
+          $(".fa-solid.fa-heart").toggle();
         });
+      });
 
       // play
       // let isPaused = true;
@@ -115,111 +140,110 @@
       //   }
       //   isPaused = !isPaused;
       // });
-      
-      
+
       let isPaused = true;
-// Bắt sự kiện click trên nút phát/pause
-btnPlay.addEventListener("click", function() {
-  btnPlay.classList.toggle("pause");
-  if (isPaused) {
-    songPlaying.play();
-  } else {
-    songPlaying.pause();
-  }
-  isPaused = !isPaused;
-});
+      // Bắt sự kiện click trên nút phát/pause
+      btnPlay.addEventListener("click", function () {
+        btnPlay.classList.toggle("pause");
+        if (isPaused) {
+          songPlaying.play();
+        } else {
+          songPlaying.pause();
+        }
+        isPaused = !isPaused;
+      });
 
-// Bắt sự kiện timeupdate của audio để cập nhật progress-bar
-songPlaying.addEventListener("timeupdate", function() {
-  const currentTime = songPlaying.currentTime;
-  const duration = songPlaying.duration;
-  const progressPercentage = (currentTime / duration) * 100;
-  progressBar.style.width = `${progressPercentage}%`;
+      // Bắt sự kiện timeupdate của audio để cập nhật progress-bar
+      songPlaying.addEventListener("timeupdate", function () {
+        const currentTime = songPlaying.currentTime;
+        const duration = songPlaying.duration;
+        const progressPercentage = (currentTime / duration) * 100;
+        progressBar.style.width = `${progressPercentage}%`;
 
-  const minutesLeft = Math.floor(currentTime / 60);
-  const secondsLeft = Math.floor(currentTime % 60);
-  const minutesRight = Math.floor(duration / 60);
-  const secondsRight = Math.floor(duration % 60);
+        const minutesLeft = Math.floor(currentTime / 60);
+        const secondsLeft = Math.floor(currentTime % 60);
+        const minutesRight = Math.floor(duration / 60);
+        const secondsRight = Math.floor(duration % 60);
 
-  // Định dạng thời gian bắt đầu và kết thúc theo dạng mm:ss
-  timeLeft.textContent = `${minutesLeft}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`;
-  timeRight.textContent = `${minutesRight}:${secondsRight < 10 ? '0' : ''}${secondsRight}`;
-});
+        // Định dạng thời gian bắt đầu và kết thúc theo dạng mm:ss
+        timeLeft.textContent = `${minutesLeft}:${
+          secondsLeft < 10 ? "0" : ""
+        }${secondsLeft}`;
+        timeRight.textContent = `${minutesRight}:${
+          secondsRight < 10 ? "0" : ""
+        }${secondsRight}`;
+      });
 
       //volume
-      btnVolume.addEventListener("input", function(e) {
-        let volume = parseFloat(e.target.value)
+      btnVolume.addEventListener("input", function (e) {
+        let volume = parseFloat(e.target.value);
         console.log(volume);
-        btnVolume.style.background = `linear-gradient(90deg, #fff ${volume - 1}%, hsla(0, 0%, 100%, 0.3) ${volume -1}%)`
+        btnVolume.style.background = `linear-gradient(90deg, #fff ${
+          volume - 1
+        }%, hsla(0, 0%, 100%, 0.3) ${volume - 1}%)`;
         songPlaying.volume = volume / 100;
         if (volume == 0) {
-          volumeIcon.classList.remove("fa-volume-low")
-          volumeIcon.classList.add("fa-volume-xmark")
-          volumeIcon.classList.remove("max-volume")
+          volumeIcon.classList.remove("fa-volume-low");
+          volumeIcon.classList.add("fa-volume-xmark");
+          volumeIcon.classList.remove("max-volume");
           // volumeIcon.onclick = () => {
           //    volumeIcon.classList.remove("fa-volume-xmark")
           //    volumeIcon.classList.add("fa-volume-low")
           //    btnVolume.value = 10;
           //    btnVolume.style.background = `linear-gradient(90deg, #fff ${volume}%, hsla(0, 0%, 100%, 0.3) ${volume}%)`
           // }
-        } 
-        else if(volume > 50) {
-          volumeIcon.classList.add("max-volume")
-          volumeIcon.classList.remove("fa-volume-xmark")
+        } else if (volume > 50) {
+          volumeIcon.classList.add("max-volume");
+          volumeIcon.classList.remove("fa-volume-xmark");
+        } else if (volume > 0 && volume <= 50) {
+          volumeIcon.classList.remove("max-volume");
+          volumeIcon.classList.add("fa-volume-low");
+          volumeIcon.classList.remove("fa-volume-xmark");
         }
-        else if(volume > 0 && volume <= 50) {
-          volumeIcon.classList.remove("max-volume")
-          volumeIcon.classList.add("fa-volume-low")
-          volumeIcon.classList.remove("fa-volume-xmark")
-        }
-      })
+      });
 
+      const progressArea = document.querySelector(".progress-area");
+      const progressBar = document.querySelector(".progress-bar");
+      let isDragging = false;
 
-
-
-
-   const progressArea = document.querySelector(".progress-area");
-    const progressBar = document.querySelector(".progress-bar");
-    let isDragging = false;
-
-    progressArea.addEventListener("mousedown", (event) => {
-      isDragging = true;
-      updateProgressBar(event);
-    });
-
-    document.addEventListener("mousemove", (event) => {
-      if (isDragging) {
-        requestAnimationFrame(() => {
-          updateProgressBar(event);
-        });
-      }
-    });
-
-    document.addEventListener("mouseup", () => {
-      isDragging = false;
-    });
-
-    progressArea.addEventListener("click", (event) => {
-      if (!isDragging) {
+      progressArea.addEventListener("mousedown", (event) => {
+        isDragging = true;
         updateProgressBar(event);
+      });
+
+      document.addEventListener("mousemove", (event) => {
+        if (isDragging) {
+          requestAnimationFrame(() => {
+            updateProgressBar(event);
+          });
+        }
+      });
+
+      document.addEventListener("mouseup", () => {
+        isDragging = false;
+      });
+
+      progressArea.addEventListener("click", (event) => {
+        if (!isDragging) {
+          updateProgressBar(event);
+        }
+      });
+
+      function updateProgressBar(event) {
+        const mouseX =
+          event.clientX - progressArea.getBoundingClientRect().left;
+        const progressBarWidth = progressArea.clientWidth;
+        const percentage = (mouseX / progressBarWidth) * 100;
+
+        if (percentage >= 0 && percentage <= 100) {
+          progressBar.style.width = `${percentage}%`;
+
+          // Cập nhật thời gian phát bài hát tương ứng với vị trí
+          const duration = songPlaying.duration;
+          const seekTime = (percentage / 100) * duration;
+          songPlaying.currentTime = seekTime;
+        }
       }
-    });
-
-    function updateProgressBar(event) {
-      const mouseX = event.clientX - progressArea.getBoundingClientRect().left;
-  const progressBarWidth = progressArea.clientWidth;
-  const percentage = (mouseX / progressBarWidth) * 100;
-
-  if (percentage >= 0 && percentage <= 100) {
-    progressBar.style.width = `${percentage}%`;
-
-    // Cập nhật thời gian phát bài hát tương ứng với vị trí
-    const duration = songPlaying.duration;
-    const seekTime = (percentage / 100) * duration;
-    songPlaying.currentTime = seekTime;
-  }
-    }
-      </script>
+    </script>
   </body>
-
-  </html>
+</html>
